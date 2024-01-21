39fbb068cd337c9109d501e333d5bb77819379d6
hiroky1983 <hirockysan1983@gmail.com>
2024-01-22 01:09:58 +0900

    Recoilを取り込み

--- src/provider/queryProvider.tsx
+++ src/provider/queryProvider.tsx
@@ -1,12 +1,14 @@
 'use client'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { ReactNode } from 'react'
+import { RecoilRoot } from 'recoil'
 
 const queryClient = new QueryClient()
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </RecoilRoot>
   )
 }
