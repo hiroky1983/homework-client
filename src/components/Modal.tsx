984147ff73ec3f4f429719c07f469884ebd47ec0
hiroky1983 <hirockysan1983@gmail.com>
2024-02-17 17:43:18 +0900

    modalのスタイリング

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,16 +8,18 @@ type Props = {
 
 export const Modal: FC<Props> = ({ onOpen, hadleClick }) => {
   return (
-    <div className="bg-gray-400 bg-opacity-50 fixed inset-0 flex justify-center items-center">
-      <div className="bg-white w-1/2 h-1/3 fixed flex justify-center items-center">
+    <div className="bg-gray-500 bg-opacity-50 fixed inset-0 flex justify-center items-center">
+      <div className="bg-white w-1/2 h-1/3 fixed flex justify-center items-center rounded-md">
         <div
-          className="absolute top-4 right-4 hover:bg-opacity-70 cursor-pointer"
+          className="absolute top-4 right-4 hover:opacity-70 cursor-pointer"
           onClick={onOpen}
         >
           x
         </div>
-        <p>本当に削除しますか？</p>
-        <Button handleClick={hadleClick}>削除</Button>
+        <div>
+          <p>本当に削除しますか？</p>
+          <Button handleClick={hadleClick}>削除</Button>
+        </div>
       </div>
     </div>
   )
