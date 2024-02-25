116cc8b6e1bef1f441c7329c8c355c0ed8f4b2de
hiroky1983 <hirockysan1983@gmail.com>
2024-02-25 20:30:10 +0900

    loadingスタイル更新

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center" aria-label="読み込み中">
+    <div
+      className="h-screen flex justify-center items-center"
+      aria-label="読み込み中"
+    >
       <div className="animate-spin h-10 w-10 border-4 border-primary rounded-full border-t-transparent"></div>
+      <div className="hidden"></div>
     </div>
   )
 }
