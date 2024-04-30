70be98da884bfd45c52b943c8c59fe508684007c
hiroky1983 <hirockysan1983@gmail.com>
2024-04-30 17:09:04 +0900

    デモ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+'use client'
+import { useEffect } from 'react'
 import { AuthScreen } from '@/screens/AuthScreen'
 
+export type Window = typeof globalThis & {
+  dataLayer: any
+}
+
+declare let window: Window
+
 export default function Home() {
+  useEffect(() => {
+    window.dataLayer.push({ user_id: 'asdfasdfasdfasdf1234213412341234' })
+  }, [])
   return (
     <>
       <AuthScreen />
