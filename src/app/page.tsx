ef120d9efdd385049899d681ad45ad099b77e1f1
hiroky1983 <hirockysan1983@gmail.com>
2024-05-06 22:03:41 +0900

    demo　revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,10 @@
 'use client'
-import { useEffect, useState } from 'react'
-import { Button } from '@/components/Button'
 import { AuthScreen } from '@/screens/AuthScreen'
 
-export type Window = typeof globalThis & {
-  dataLayer: any
-}
-
-declare let window: Window
-
 export default function Home() {
-  const [userId, setUserId] = useState('')
-  const [customerId, setCustomerId] = useState('')
-
-  const rand1 = () => {
-    return String(Math.floor(Math.random() * 1000000))
-  }
-
-  const rand2 = () => {
-    return String(Math.floor(Math.random() * 1000000))
-  }
-
-  useEffect(() => {
-    window.dataLayer.push({
-      user_id: userId,
-      customer_id: customerId,
-    })
-
-    console.log('userId', userId)
-    console.log('customerId', customerId)
-    console.log('dataLayer', window.dataLayer)
-  }, [customerId, userId])
   return (
     <>
       <AuthScreen />
-      <Button
-        handleClick={() => {
-          setUserId(rand1())
-          setCustomerId(rand2())
-          window.dataLayer.push({
-            event: 'page_view',
-          })
-        }}
-      >
-        ろぐいん
-      </Button>
     </>
   )
 }
